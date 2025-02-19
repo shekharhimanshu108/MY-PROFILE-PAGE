d50f13a37bc59475ab38f800f0ec28a5ba1ce6d9
shekharhimanshu108 <shekharhimanshu1511@gmail.com>
2025-02-19 21:48:20 +0530

    changing

--- MY PROFILE/my-profile/src/card.jsx
+++ MY PROFILE/my-profile/src/card.jsx
@@ -7,7 +7,7 @@ function Card(){
              <div className="Card">
                 <img className='dp' src={profilepic} alt="profile picture" />
                 <h2>Hey there, this is Himanshu Shekhar</h2>
-                <p>I am a no one and everyone at the same time</p>
+                <p>I am a danger no one and everyone at the same time</p>
              </div>
 
     );
